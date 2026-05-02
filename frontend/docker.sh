86782ea70095f3a2ab67e59b623c7b8081b72dfb
Mohammed Amine Mahdam <momahdam@e2r4p15.1337.ma>
2026-05-02 18:13:45 +0100

    c

--- frontend/docker.sh
+++ frontend/docker.sh
@@ -1,4 +1,5 @@
 #! bin/bash
+
 echo "Docker is not dockering"
 docker build -t est-chatbot .
 docker run -p 5173:5173 est-chatbot
--- frontend/run.sh
+++
@@ -1,6 +0,0 @@
-#! bin/bash
-
-echo "Docker is not dockering"
-docker build -t est-chatbot .
-docker run -p 5173:5173 est-chatbot
-lsof -i :5173
