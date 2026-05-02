f2ac19af111ae5f119197bf60a5a902823495587
Aimran <medamin.mahdam@gmail.com>
2026-05-02 18:24:36 +0100

    Docker edit

--- frontend/docker.sh
+++ frontend/docker.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
 echo "Docker is not dockering"
-docker build -t est-chatbot .
-docker run -p 5173:5173 est-chatbot
+docker build -t est_chatbot .
+docker run -p 5173:5173 est_chatbot
 lsof -i :5173
