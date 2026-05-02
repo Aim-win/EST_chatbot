ea10ae8a194292a07217b83548cabf13a906e585
Mohammed Amine Mahdam <momahdam@e2r4p15.1337.ma>
2026-05-02 18:05:35 +0100

    Docker is not dockering

---
+++ frontend/run.sh
@@ -0,0 +1,6 @@
+#! bin/bash
+
+echo "Docker is not dockering"
+docker build -t est-chatbot .
+docker run -p 5173:5173 est-chatbot
+lsof -i :5173
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
-    // Proxy API calls to your backend during development
     proxy: {
       '/chat': {
         target: 'http://localhost:8000',
